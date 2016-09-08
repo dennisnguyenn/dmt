7a4c53a69bf7d331f3a4edf88500912567196892
denni_000 <dennis.nguyen@student.rocvf.nl>
2016-09-08 23:18:21 +0200

    H04

---
+++ src/hoofdstuk4/H041.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.applet.*;
+
+public class H041 extends Applet{
+    public void paint(Graphics gh)
+    {
+        gh.drawLine(40,180,150,10);
+        gh.drawLine(260,180,150,10);
+        gh.drawRect(40,180,221,0);
+    }
+
+}
---
+++ src/hoofdstuk4/H042.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.applet.*;
+
+public class H042 extends Applet{
+    public void paint(Graphics gh)
+    {
+        gh.drawLine(40,180,150,10);
+        gh.drawLine(260,180,150,10);
+        gh.drawRect(40,180,221,0);
+
+        gh.drawRect(80,180,150,150);
+        gh.drawRect(180,280,15,40);
+        gh.drawRect(100,200,50,15);
+
+    }
+
+}
