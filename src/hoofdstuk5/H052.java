76e276f670d3432ab55aeb49d43727c21044022c
dennisnguyenn <dennis.nguyen@student.rocvf.nl>
2016-09-27 09:27:34 +0200

    Update H052.java

--- src/hoofdstuk5/H052.java
+++ src/hoofdstuk5/H052.java
@@ -5,29 +5,27 @@ import java.applet.*;
 
 public class H052 extends Applet {
 
-    public void init (){}
-    public void paint (Graphics g){
+    public void init() {
+    }
 
-    int x = 60;
-    int y = 155;
+    int valerie = 40;
+    int jeroen = 100;
+    int hans = 80;
+    int hoogtexas = 200;
+    int v = hoogtexas - valerie;
+    int j = hoogtexas - jeroen;
+    int h = hoogtexas - hans;
 
-
-        g.drawString("100",40,40);
-        g.drawString("80",40,70);
-        g.drawString("60",40,100);
-        g.drawString("40",40,130);
-        g.drawString("20",40,160);
-        g.drawLine(37, 185, 37, 35);
-        g.drawString("Valerie", 70, 201);
-        g.drawString("Jeroen", 140, 201);
-        g.drawString("Hans", 210, 201);
-        g.setColor(Color.yellow);
-        g.fillRect(70, 124, x, 66);
-        g.setColor(Color.blue);
-        g.fillRect(140, 35, x, 155);
-        g.setColor(Color.red);
-        g.fillRect(210, 70, x, 120);
-
+    public void paint(Graphics g) {
+        g.drawLine(200, 50, 200, 200);
+        g.drawLine(350, 200, 200, 200);
+        g.fillRect(220, v,30, valerie);
+        g.fillRect(270, j, 30,jeroen);
+        g.fillRect(320, h, 30,hans);
+        g.drawString("Valerie", 220, 215);
+        g.drawString("Jeroen", 270, 215);
+        g.drawString("Hans", 320, 215);
+        g.drawString("0", 180, 200);
+        g.drawString("150", 175, 60);
     }
 }
-
